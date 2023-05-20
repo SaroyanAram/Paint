03a7dfd670899458fd055a5dec6e61e6b0b6afd1
Saroyan Aram <aramsaroan02687@gmail.com>
2023-05-20 16:47:11 +0700

    ImageView size lock

--- app/src/main/java/com/example/paint/PickImageActivity.java
+++ app/src/main/java/com/example/paint/PickImageActivity.java
@@ -188,7 +188,7 @@ public class PickImageActivity extends AppCompatActivity implements View.OnClick
                 .saveDir(getExternalFilesDir(Environment.DIRECTORY_PICTURES))
                 //.crop()	    			//Crop image(Optional), Check Customization for more option
                 //.compress(1024)			//Final image size will be less than 1 MB(Optional)
-                //.maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
                 .start();
 
     }
